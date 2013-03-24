daa1fd48adc75cd5a32d4e57361049d74193fbd7
Radoslav Gerganov <rgerganov@gmail.com>
2013-03-24 14:49:44 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=`dirname ${BASH_SOURCE[0]}`
+cd $DIR
+git submodule init
+git submodule update
+if [ -a ~/.vim ]; then
+    echo "~/.vim already exists"
+else
+    ln -s `pwd`/.vim ~/.vim
+fi
+ln -f -s `pwd`/.vimrc ~/.vimrc
+ln -f -s `pwd`/.gitconfig ~/.gitconfig
+ln -f -s `pwd`/.ackrc ~/.ackrc
+ln -f -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -f -s `pwd`/.sqshrc ~/.sqshrc
+
